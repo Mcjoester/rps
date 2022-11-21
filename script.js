0974f91c1a9f155f33e38a58b779aa26cc005d1d
Jose <jpelayo53@gmail.com>
2022-11-20 22:18:12 -0700

    added index.html reset.css script.js style.css

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Scores for game
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll('input');
+
+// If user selects a button, run a round
+const buttons = document.querySelectorAll('#buttons button')
+for (let button of buttons) {
+    button.addEventListener('click', game)
+}
+
+function game() {
+    const result = playRound(this.className, computerPlay())
+    updateScore(result)
+}
+
+function updateScore (result) {
+    const liveScore = document.querySelector('h2 span')
+    liveScore.innerText = `${playerScore} - ${computerScore}`
+}
+
 
 // Array for computer to choose between Rock, Paper, and Scissors
 let options = ["Rock", "Paper", "Scissors"];
@@ -11,6 +27,15 @@ function computerPlay() {
     return options[Math.floor(Math.random()*options.length)];
 }
 
+function displayMoves(playerSelection, computerSelection) {
+    const playerClasses = document.querySelector('#player .image').classList
+    playerClasses.remove(playerClasses.item(1))
+    playerClasses.add(playerSelection)
+
+    const cpuClasses = document.querySelector('#computer .image').classList
+    cpuClasses.remove(cpuClasses.item(1))
+    cpuClasses.add(computerSelection)
+}
 
 // Disable button function
 function disableButtons() {
@@ -19,19 +44,21 @@ function disableButtons() {
     })
 }
 
+
 //Function to play a round of Rock Paper Scissors 
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
     let result = "";
 
-
+    displayMoves(playerSelection, computerSelection)
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
     (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
     (playerSelection == 'Paper' && computerSelection == 'Rock')) {
     
     playerScore += 1
     result = 'Computer chose: ' + computerSelection + '<br><br>You chose: ' + playerSelection + 
-    '<br><br> You win! ' + playerSelection + ' beats ' + computerSelection + '<br><br>Player score: ' + playerScore + '<br>Computer score: ' + computerScore;
+    '<br><br> You win! ' + playerSelection + ' beats ' + computerSelection;
+
 
     if (playerScore == 5) {
         result += '<br><br>You won the game! Reload the page to play again'
@@ -39,13 +66,11 @@ function playRound(playerSelection) {
     }
 }
 else if (playerSelection == computerSelection) {
-    result = ('Computer chose: ' + computerSelection + '<br><br>You chose: ' + playerSelection + '<br><br>It\'s a tie. You both chose ' + playerSelection
-        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    result = ('Computer chose: ' + computerSelection + '<br><br>You chose: ' + playerSelection + '<br><br>It\'s a tie. You both chose ' + playerSelection)
 }
 else {
     computerScore += 1
-    result = ('Computer chose: ' + computerSelection + '<br><br>You chose: ' + playerSelection + '<br><br>You lose! ' + computerSelection + ' beats ' + playerSelection
-        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    result = ('Computer chose: ' + computerSelection + '<br><br>You chose: ' + playerSelection + '<br><br>You lose! ' + computerSelection + ' beats ' + playerSelection)
 
     if (computerScore == 5) {
         result += '<br><br>I won the game! Reload the page to play again'
@@ -53,13 +78,20 @@ else {
     }
 }
 
-document.getElementById('result').innerHTML = result
-return
+
+
+    document.getElementById('result').innerHTML = result
+    return
+
+    
 }
 
 
-buttons.forEach(button =>{
-button.addEventListener('click', function(){
-    playRound(button.value)
-})
-})
+
+
+
+// Event Listeners
+//buttons.forEach(button =>
+//button.addEventListener('click', playRound));
+//console.log(playerSelection, computerSelection);
+
